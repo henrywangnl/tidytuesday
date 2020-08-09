3b3a57e0f3528df460f9e8dacccd205989ce6252
Henry <henrywangnl@gmail.com>
2020-08-09 21:54:30 +0200

    remove unfinished files

--- cocktails.R
+++
@@ -1,4 +0,0 @@
-
-boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
-skimr::skim(boston_cocktails)
-View(boston_cocktails)
